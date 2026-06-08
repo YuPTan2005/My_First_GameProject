c02f15ac2f0059c4732e6f00276441a5502f182d
YuP35315601 <ytan0378@student.monash.edu>
2026-06-08 12:55:51 +1000

    Created the anim instance class for the enemy's animation and modified the blueprint for that class in unreal editor

---
+++ Source/D4_HD3_CustomProject/EnemyAnimInstance.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAnimInstance.h"
+
+void UEnemyAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+	
+	Character = Cast<AEnemy>(TryGetPawnOwner());
+}
+
+void UEnemyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (Character)
+	{
+		Speed = Character->GetVelocity().Length();
+		bCanAttack = Character->bCanAttack;
+		bIsDead = Character->bIsDead;
+	}
+}
---
+++ Source/D4_HD3_CustomProject/EnemyAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Animation/AnimInstance.h"
+#include "EnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D4_HD3_CUSTOMPROJECT_API UEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemy* Character;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Speed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bCanAttack;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bIsDead;
+	
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+};
