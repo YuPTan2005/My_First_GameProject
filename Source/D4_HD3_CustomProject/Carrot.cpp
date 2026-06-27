8365ec197a5a2af53d27329dd307bce6e61cc514
YuP35315601 <ytan0378@student.monash.edu>
2026-06-27 16:21:24 +1000

    Added new food - Create new class for carrot - Modified inventory widget blueprint - Added a new spawning location for carrot

---
+++ Source/D4_HD3_CustomProject/Carrot.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Carrot.h"
+
+// Sets default values
+ACarrot::ACarrot()
+{
+	FoodName = "Carrot";
+	FoodDescription = "Increase your experience amount by 50 and starvation amount by 25.";
+	ExperienceAmount = 50;
+	StarvationAmount = 25;
+}
+
---
+++ Source/D4_HD3_CustomProject/Carrot.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Food.h"
+#include "GameFramework/Actor.h"
+#include "Carrot.generated.h"
+
+UCLASS()
+class D4_HD3_CUSTOMPROJECT_API ACarrot : public AFood
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACarrot();
+
+};
