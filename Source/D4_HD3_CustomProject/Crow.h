5f66dfe248ea2e1478fd9d5b08f60200d44d57b8
YuP35315601 <ytan0378@student.monash.edu>
2026-06-28 13:28:37 +1000

    Created new enemy type class for crow

---
+++ Source/D4_HD3_CustomProject/Crow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Crow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D4_HD3_CUSTOMPROJECT_API ACrow : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
