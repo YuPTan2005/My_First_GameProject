df0c4c92a804ce14f79d7e457ca3bc68d5e57311
YuP35315601 <ytan0378@student.monash.edu>
2026-06-28 18:41:09 +1000

    Created new class for new food type and created blueprint for it

---
+++ Source/D4_HD3_CustomProject/Pineapple.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pineapple.h"
+
+APineapple::APineapple()
+{
+	FoodName = "Pineapple";
+	FoodDescription = TEXT("Increase your experience amount by 20 and starvation value to max. ")
+					  TEXT("Starvation value will stop to drop for 20 seconds.");
+	ExperienceAmount = 20;
+	StarvationAmount = 0; // Will directly set the value to max
+}
+
+void APineapple::EatenBy_Implementation(ACharacter* Character)
+{
+	Super::EatenBy_Implementation(Character);
+	
+	Player = Cast<AD4_HD3_CustomProjectCharacter>(Character);
+	if (Player)
+	{
+		Player->StarvationValue = Player->MaxStarvationValue;
+		Player->bIsStarvationDecrement = false;
+		GetWorld()->GetTimerManager().SetTimer(
+			StarvationTimer,
+			this,
+			&APineapple::ContinueStarvationDecrement,
+			EffectDuration,
+			false
+			);
+	}
+}
+
+void APineapple::ContinueStarvationDecrement()
+{
+	Player->bIsStarvationDecrement = true;
+}
+
+
---
+++ Source/D4_HD3_CustomProject/Pineapple.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D4_HD3_CustomProjectCharacter.h"
+#include "Food.h"
+#include "Pineapple.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D4_HD3_CUSTOMPROJECT_API APineapple : public AFood
+{
+	GENERATED_BODY()
+	
+public:
+	APineapple();
+	
+	virtual void EatenBy_Implementation(ACharacter* Character) override;
+	
+protected:
+	AD4_HD3_CustomProjectCharacter* Player;
+	
+	FTimerHandle StarvationTimer;
+	float EffectDuration = 20.0f;
+	void ContinueStarvationDecrement();
+	
+};
