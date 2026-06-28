c2de00e001ed990cea3eb3372bd2248c1a3cf09a
YuP35315601 <ytan0378@student.monash.edu>
2026-06-28 13:25:54 +1000

    Removed unnecessary code for Owl class

--- Source/D4_HD3_CustomProject/Owl.h
+++ Source/D4_HD3_CustomProject/Owl.h
@@ -11,19 +11,4 @@ class D4_HD3_CUSTOMPROJECT_API AOwl : public AEnemy
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
-	AOwl();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
