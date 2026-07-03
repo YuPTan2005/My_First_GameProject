1da010289bcd9c14fa102e9faa3539bfee83cad1
YuP35315601 <ytan0378@student.monash.edu>
2026-07-03 19:03:20 +1000

    Removed attackable interface and changed the parameter type of deal damage function in Damageable interface

--- Source/D4_HD3_CustomProject/Damageable.h
+++ Source/D4_HD3_CustomProject/Damageable.h
@@ -24,6 +24,6 @@ class D4_HD3_CUSTOMPROJECT_API IDamageable
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
-	void DealDamage(float DamageTaken, IAttackable* DamagedBy);
+	void DealDamage(float DamageTaken, AActor* DamagedBy);
 	
 };
