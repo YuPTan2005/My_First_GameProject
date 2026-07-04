6d153bac6b011feaf585d869bc25d68d4349766e
YuP35315601 <ytan0378@student.monash.edu>
2026-07-04 14:39:46 +1000

    Created an interface for the controllers - Declared the UpdateAttackCheck and Attack functions

---
+++ Source/D4_HD3_CustomProject/AttackBTInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttackBTInterface.h"
+
+// Add default functionality here for any IAttackBTInterface functions that are not pure virtual.
