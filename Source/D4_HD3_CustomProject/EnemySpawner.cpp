3807b759eaf4aa41dec3b1ac50a00902ce5a639d
YuP35315601 <ytan0378@student.monash.edu>
2026-07-13 23:03:47 +1000

    Updated the for loop logic of OnGameStarted function in EnemySpawner.cpp

--- Source/D4_HD3_CustomProject/EnemySpawner.cpp
+++ Source/D4_HD3_CustomProject/EnemySpawner.cpp
@@ -73,7 +73,7 @@ bool AEnemySpawner::SpawnEnemy()
 
 void AEnemySpawner::OnGameStarted()
 {
-	for (int i=0; i<=GameStartEnemyNumber; i++)
+	for (int i=1; i<=GameStartEnemyNumber; i++)
 	{
 		SpawnEnemy();
 	}
