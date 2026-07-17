6c895805a11703bcef133bee7162b8e624023048
YuP35315601 <ytan0378@student.monash.edu>
2026-07-17 17:38:37 +1000

    Added an interface for the chest item

---
+++ Source/D4_HD3_CustomProject/ChestItem.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChestItem.h"
+
+// Add default functionality here for any IChestItem functions that are not pure virtual.
