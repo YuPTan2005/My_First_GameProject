ef49cdd9ffd7235ab06f8f45909d290929826a4b
YuP35315601 <ytan0378@student.monash.edu>
2026-07-22 21:49:30 +1000

    Created new interface for actors that can deal more damage

---
+++ Source/D4_HD3_CustomProject/ExtraDamageDealer.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExtraDamageDealer.h"
+
+// Add default functionality here for any IExtraDamageDealer functions that are not pure virtual.
