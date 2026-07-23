e7f526437e3c1acfd1252e8dfd84af2a203cd509
YuP35315601 <ytan0378@student.monash.edu>
2026-07-23 10:25:07 +1000

    Changed the key to open food inventory widget to V

--- Source/D4_HD3_CustomProject/InventoryWidget.cpp
+++ Source/D4_HD3_CustomProject/InventoryWidget.cpp
@@ -30,7 +30,7 @@ FReply UInventoryWidget::NativeOnKeyDown(const FGeometry& InGeometry, const FKey
 {
 	FKey PressedKey = InKeyEvent.GetKey();
 	
-	if (PressedKey == EKeys::B)
+	if (PressedKey == EKeys::V)
 	{
 		if (Owner)
 		{
