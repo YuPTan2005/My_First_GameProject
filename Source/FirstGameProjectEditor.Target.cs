05980d94027940251c7f93b179f93a66334621ae
YuP35315601 <ytan0378@student.monash.edu>
2026-07-28 22:13:39 +1000

    Updated the unreal engine version used by this project

--- Source/FirstGameProjectEditor.Target.cs
+++ Source/FirstGameProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class FirstGameProjectEditorTarget : TargetRules
 	public FirstGameProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("D4_HD3_CustomProject");
 	}
